6375754ba02c9acdcb2cd8d0c8f1d2edb18086f5
Tommy Parnell <tommy@terribledev.io>
2021-12-28 13:57:26 -0500

    init

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+// #[launch]
+// fn rocket() -> _ {
+//     // let cfg =  rocket::config::Config
+//     // .address("0.0.0.0")
+//     // .port(port)   
+//     // .unwrap();
+//     // rocket::build()
+//     //.configure(cfg)
+//     // .mount("/", routes![index])
+//     // .launch()
+//     rocket::ignite().mount("/", routes![index]).launch();
+// }
+#[launch]
+fn rocket() -> _ {
+    let port = std::env::var("PORT").unwrap_or("8000".to_string());
+    let port_as_int = port.parse::<u16>().unwrap();
+    let figmant = rocket::Config::figment().merge(("port", port_as_int));
+
+    rocket::custom(figmant).mount("/", routes![index])
+}
